ca2009869124467087db2b744f6ef4f5cf875acc
Jin Pan <jinxp1@gmail.com>
2014-08-29 03:08:55 -0400

    + referrals

--- js/mit_midway.js
+++ js/mit_midway.js
@@ -1,35 +1,46 @@
-function MasterControl($scope) {
+function MasterControl($scope, $interval) {
     $scope.kerberoses = [];
+    $scope.referrers = [];
 
     $scope.kerberos = "";
     $scope.last_kerberos = "";
+    $scope.referrer = "";
 
     $scope.init = function() {
+        $scope.update();
+        $interval($scope.update, 1000);
+    }
+
+    $scope.update = function() {
         var kerberoses = localStorage.getItem("kerberoses");
+        var referrers = localStorage.getItem("referrers");
         if (kerberoses === null) {
             $scope.kerberoses = [];
+            $scope.referrers = [];
         } else {
             $scope.kerberoses = jQuery.parseJSON(kerberoses);
+            $scope.referrers = jQuery.parseJSON(referrers);
         }
     }
 
     $scope.enter = function(keyEvent) {
         if ((keyEvent === undefined) || (keyEvent.which === 13)) {
 
-            if ($scope.isValidKerberos()) {
+            if ($scope.isValidKerberos($scope.kerberos)) {
                 // success!
-                $scope.save($scope.kerberos);
+                $scope.save($scope.kerberos, $scope.referrer);
 
                 $scope.last_kerberos = $scope.kerberos;
                 $scope.kerberos = "";
+                $scope.referrer = "";
 
                 jQuery('#success').dimmer('show');
             }
         }
     }
 
-    $scope.isValidKerberos = function() {
-        var kerberos = $scope.kerberos.toLowerCase();
+    $scope.isValidKerberos = function(kerberos) {
+        var kerberos = kerberos.toLowerCase();
         if (kerberos.length < 3 || kerberos.match(/^[a-z][a-z0-9]*$/) === null) {
             return false;
         }
@@ -39,19 +50,18 @@ function MasterControl($scope) {
         return (jQuery.inArray(hash, hashes) >= 0)
     }
 
-    $scope.save = function(kerberos) {
-        var kerberoses;
-        if (localStorage.getItem("kerberoses") === null) {
-            kerberoses = [];
-        } else {
-            kerberoses = jQuery.parseJSON(localStorage.getItem("kerberoses"));
+    $scope.save = function(kerberos, referrer) {
+        if (localStorage.getItem("kerberoses") !== null) {
+            $scope.kerberoses = jQuery.parseJSON(localStorage.getItem("kerberoses"));
+            $scope.referrers = jQuery.parseJSON(localStorage.getItem("referrers"));
         }
 
-        if (jQuery.inArray(kerberos, kerberoses) < 0) {
-            kerberoses.push(kerberos);
-            localStorage.setItem("kerberoses", JSON.stringify(kerberoses));
+        if (jQuery.inArray(kerberos, $scope.kerberoses) < 0) { // not already added
+            $scope.kerberoses.push(kerberos);
+            $scope.referrers.push(referrer);
+
+            localStorage.setItem("kerberoses", JSON.stringify($scope.kerberoses));
+            localStorage.setItem("referrers", JSON.stringify($scope.referrers));
         }
     }
-
 }
-
