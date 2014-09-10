21002fd8cee330c2107a125fc23879047f2f2fc5
Jin Pan <jinxp1@gmail.com>
2014-09-10 18:29:13 -0400

    remove refer

--- js/mit_midway.js
+++ js/mit_midway.js
@@ -1,10 +1,8 @@
 function MasterControl($scope, $interval, $timeout) {
     $scope.kerberoses = [];
-    $scope.referrers = [];
 
     $scope.kerberos = "";
     $scope.last_kerberos = "";
-    $scope.referrer = "";
 
     $scope.init = function() {
         $scope.update();
@@ -13,13 +11,10 @@ function MasterControl($scope, $interval, $timeout) {
 
     $scope.update = function() {
         var kerberoses = localStorage.getItem("kerberoses");
-        var referrers = localStorage.getItem("referrers");
         if (kerberoses === null) {
             $scope.kerberoses = [];
-            $scope.referrers = [];
         } else {
             $scope.kerberoses = jQuery.parseJSON(kerberoses);
-            $scope.referrers = jQuery.parseJSON(referrers);
         }
     }
 
@@ -31,17 +26,16 @@ function MasterControl($scope, $interval, $timeout) {
             if ($scope.isValidKerberos($scope.kerberos)
                 || keyEvent === undefined) {  // either is valid or click to override
                 // success!
-                $scope.save($scope.kerberos, $scope.referrer);
+                $scope.save($scope.kerberos);
 
                 $scope.last_kerberos = $scope.kerberos;
                 $scope.kerberos = "";
-                $scope.referrer = "";
 
                 jQuery('#success').dimmer('show');
                 
                 $timeout(function() {
                     jQuery('#success').dimmer('hide');
-                }, 1000);
+                }, 2000);
             }
         }
     }
@@ -57,18 +51,15 @@ function MasterControl($scope, $interval, $timeout) {
         return (jQuery.inArray(hash, hashes) >= 0)
     }
 
-    $scope.save = function(kerberos, referrer) {
+    $scope.save = function(kerberos) {
         if (localStorage.getItem("kerberoses") !== null) {
             $scope.kerberoses = jQuery.parseJSON(localStorage.getItem("kerberoses"));
-            $scope.referrers = jQuery.parseJSON(localStorage.getItem("referrers"));
         }
 
         if (jQuery.inArray(kerberos, $scope.kerberoses) < 0) { // not already added
             $scope.kerberoses.push(kerberos);
-            $scope.referrers.push(referrer);
 
             localStorage.setItem("kerberoses", JSON.stringify($scope.kerberoses));
-            localStorage.setItem("referrers", JSON.stringify($scope.referrers));
         }
     }
 }
